e77afe6d4d50d3f16d2ac21cdefaf79ce7e987a6
nguyenbaongoc123 <127759913+nguyenbaongoc123@users.noreply.github.com>
2023-03-31 20:23:46 +0900

    Create AreaOfTriangle.py

---
+++ AreaOfTriangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+a = float(input())
+b = float(input())
+c = float(input())
+s = (a+b+c)/2
+area = math.sqrt((s-a)*(s-b)*(s-c)*s)
+print(" Area of the triangle is: ", area)
+
+
+# In[ ]:
+
+
+
+
